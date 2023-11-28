5c41c1225c06da6d2a695f16a943d5db7521e015
India Cornell <indiaccornell@gmail.com>
2023-11-28 13:40:55 -0800

    Update class Question

---
+++ classes/question.rb
@@ -0,0 +1,25 @@
+class Question
+    # - Instance variables:
+  #   - num1
+  #   - num2
+  #   - answer
+  # - Methods:
+  #   - ask_question
+  #   - check_answer
+  attr_reader :num1, :num2, :answer
+  
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = @num1 + @num2
+  end
+  
+  def ask_question
+    puts "What is the sum of #{@num1} + #{@num2}?"
+  end
+  
+  def check_answer(player_answer)
+    player_answer == @answer ? true : false
+  end
+end
+  
