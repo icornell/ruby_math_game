fa41f4739879b27a0fc631175b2e1b70059cce86
India Cornell <indiaccornell@gmail.com>
2023-11-28 13:31:04 -0800

    setting up main.rb file

---
+++ main.rb
@@ -0,0 +1,4 @@
+require_relative 'game' # require_relative is used to require files relative to the current file's path
+
+game = Game.new # create a new instance of the Game class
+game.start_game # start_game is an instance method of the Game class
