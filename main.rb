bf3f4df308d3fd05cef32e0fed197c898eb3268c
India Cornell <indiaccornell@gmail.com>
2023-11-28 15:12:16 -0800

    Update main for full gameplay with correct require statements

--- main.rb
+++ main.rb
@@ -1,4 +1,7 @@
-require_relative 'game' # require_relative is used to require files relative to the current file's path
+require_relative 'classes/game'
+require_relative 'classes/player'
+require_relative 'classes/question'
+
 
 game = Game.new # create a new instance of the Game class
 game.start_game # start_game is an instance method of the Game class
